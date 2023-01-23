4844dcb7a596bfa6b912f9302e7ce750164cb1b4
jjenus <biz.jenus@gmail.com>
2023-01-23 17:14:32 +0100

    updated live chat

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,15 +1,5 @@
 <script setup>
 	import { RouterView } from "vue-router";
-
-	window.$crisp = [];
-	window.CRISP_WEBSITE_ID = "0a669860-f587-4fce-884b-6856e86f1971";
-	(function () {
-		const d = document;
-		const s = d.createElement("script");
-		s.src = "https://client.crisp.chat/l.js";
-		s.async = 1;
-		d.getElementsByTagName("head")[0].appendChild(s);
-	})();
 </script>
 
 <template>
