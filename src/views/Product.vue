b3615b42f5ce7b69222fa4e9f48ea2554725becc
jjenus <biz.jenus@gmail.com>
2023-01-23 17:49:38 +0100

    moved chat to only product pages

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,5 +1,20 @@
 <script setup>
+	import { onMounted } from "vue";
 	import { RouterView } from "vue-router";
+
+	async function mountChat() {
+		const plugin = document.createElement("script");
+		plugin.setAttribute(
+			"src",
+			"//code.tidio.co/oje9p3zweqxnqqcawyqj6y0krt7adfbb.js"
+		);
+		plugin.async = true;
+		document.head.appendChild(plugin);
+	}
+
+	onMounted(() => {
+		mountChat();
+	});
 </script>
 
 <template>
